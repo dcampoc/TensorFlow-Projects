31c6cf639e87911f1fe0c4a294aae44edd10e114
dcampoc <dcampoc@gmail.com>
2020-08-08 00:11:52 +0200

    Adding README file and improving exercise_2.py

---
+++ Object_Oriented_Programming/README.txt
@@ -0,0 +1,4 @@
+For observing the actions/functionalities of the exercises, the following steps must be guaranteed:
+
+1) The files 'log.txt' and 'text.csv' should be empty. (They are modified through 'exercise_2.py')
+2) The file 'test_exercise_3.txt' should have initialy the same form of 'test_exercise_3_backup.txt' 
--- Object_Oriented_Programming/exercise_2.py
+++ Object_Oriented_Programming/exercise_2.py
@@ -60,65 +60,8 @@ x.set_val(1)
 print(x.get_val())
 print('MyClass is of the type: ' + str(type(MyClass)))
 
-import datetime
 
-class WriteFile(object):
-    def __init__(self, name_file):
-        self.name_file = name_file
-        
-    def write(self, text_input):
-        self.text_input = text_input
-        file_write = open(self.name_file,'w')
-        file_write.write(self.text_input)
-        file_write.close()
-        print(self.text_input)
-
-    @staticmethod
-    def list_verify(text_input):
-        if (type(text_input) != list):
-            raise TypeError('The input must be a list')
-        return text_input
-    
-    @staticmethod
-    def str_verify(text_input):
-        if (type(text_input) != str):
-            raise TypeError('The input must be a list')
-        return text_input 
-    
-class LogFile(WriteFile):
-    def __init__(self, name_file):
-        WriteFile.__init__(self, name_file)
-    
-    def write(self, text_input):
-        text_input = WriteFile.str_verify(text_input)
-        text_input = datetime.datetime.now().strftime('%Y-%m-%d %H:%M') + ' '*4 + text_input
-        WriteFile.write(self,text_input)
-        
-class DeLimFile(WriteFile):
-    def __init__(self, name_file, delimeter):
-        WriteFile.__init__(self, name_file)
-        self.delimeter = ','
-        
-    def write(self, text_input):
-        text_input = WriteFile.list_verify(text_input)
-        output = ''
-        for i in range(len(text_input)):
-            # if ',' in text_input[i]:
-            #     text_input[i] = "'{0}'".format(text_input[i])
-            output = output + text_input[i] + self.delimeter
-        output = output[:-1]
-        WriteFile.write(self,output)
-
-
-log = LogFile('log.txt')
-c = DeLimFile('text.csv', ',')
-
-c.write(['a', 'b', 'c', 'd'])
-c.write(['1', '2,', '3', '4'])
-
-log.write('this is a log message')
-log.write('this is another log message')
-
+#################################################################################################################
 # Class that allows summing the numerical content of two lists (without using other packages such as numpy or pandas)
 class SumList(object): # class SumList(list): would inherit the all list's methods
     
@@ -141,6 +84,121 @@ list_b = SumList([10,20,30,40,50,60,70,80, 90])
 
 list_ab = list_a + list_b
 print(list_ab.this_list)
+
+###################################################################################
+
+'''
+More complex exercise: Use the inheritance property of classes for creating two subclasses that allow to write data to .txt and .csv formats
+'''
+
+# Proposed solution (it checks for the different types of data required for the application and allows writing ',' symbols in the .csv file)
+import datetime
+
+class WriteFile(object):
+    def __init__(self, name_file):
+        self.name_file = name_file
+        
+    def write(self, text_input):
+        self.text_input = text_input
+        file_write = open(self.name_file,'w')
+        file_write.write(self.text_input + '\n')
+        file_write.close()
+        print(self.text_input)
+
+    @staticmethod
+    def list_verify(text_input):
+        if (type(text_input) != list):
+            raise TypeError('The input must be a list')
+        return text_input
+    
+    @staticmethod
+    def str_verify(text_input):
+        if (type(text_input) != str):
+            raise TypeError('The input must be a list')
+        return text_input 
+    
+class LogFile(WriteFile):
+    
+    def write(self, text_input):
+        text_input = WriteFile.str_verify(text_input)
+        text_input = datetime.datetime.now().strftime('%Y-%m-%d %H:%M') + ' '*4 + text_input
+        WriteFile.write(self,text_input)
+        
+class DeLimFile(WriteFile):
+    def __init__(self, name_file, delimeter):
+        WriteFile.__init__(self, name_file)
+        self.delimeter = ','
+        
+    def write(self, text_input):
+        text_input = WriteFile.list_verify(text_input)
+        output = ''
+        text_input_final = []
+        for i in range(len(text_input)):
+            if ',' in text_input[i]:
+                text_input[i] = '"{0}"'.format(text_input[i])
+            text_input_final.append(text_input[i])
+        
+        output = self.delimeter.join(text_input_final)
+        WriteFile.write(self,output)
+
+
+log = LogFile('log.txt')
+c = DeLimFile('text.csv', ',')
+
+c.write(['a', 'b', 'c', 'd'])
+c.write(['1', '2,', '3', '4'])
+
+log.write('this is a log message')
+log.write('this is another log message')
+
+################################################################################################################
+
+# Solution of the assignment (Cleaner and simpler version of the algorithm with basic functionalities)
+import abc
+
+class WriteFileSol(object):
+    
+    __metaclass__ = abc.ABCMeta
+    
+    @abc.abstractmethod
+    def write(self):
+        return 
+    
+    def __init__(self, filename):
+        self.filename = filename
+        
+    def write_line(self, text):
+        fh = open(self.filename, 'a')
+        fh.write(text + '\n')
+        fh.close()
+        
+class DeLimFileSol(WriteFileSol):
+    
+    def __init__(self,filename, delim):
+        WriteFileSol.__init__(self, filename)
+        self.delim = delim
+        
+    def write(self, this_list):
+        line = self.delim.join(this_list)
+        self.write_line(line)
+        
+class LogFileSol(WriteFileSol):
+    
+    def write(self, this_line):
+        dt =  datetime.datetime.now().strftime('%Y-%m-%d %H:%M')
+        self.write_line('{0}    {1}'.format(dt, this_line))
+        
+        
+# log = LogFileSol('log.txt')
+# c = DeLimFileSol('text.csv', ',')
+
+# c.write(['a', 'b', 'c', 'd'])
+# c.write(['1', '2,', '3', '4'])
+
+# log.write('this is a log message')
+# log.write('this is another log message')
+
+
 
 
 
